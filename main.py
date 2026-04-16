534a2bd235d5c45d6bc3c4c6c88d94d77baa73a2
NaoTomori0 <kyongfam0@gmail.com>
2026-04-16 18:52:11 +0300

    upd

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import time
 def intersect_cube(rox, roy, roz, rdx, rdy, rdz, size):
     eps = 1e-7
 
-    # Считаем плоскости (просто числа)
     tx1 = (-size - rox) / (rdx + eps)
     tx2 = (size - rox) / (rdx + eps)
     ty1 = (-size - roy) / (rdy + eps)
@@ -16,7 +15,6 @@ def intersect_cube(rox, roy, roz, rdx, rdy, rdz, size):
     tz1 = (-size - roz) / (rdz + eps)
     tz2 = (size - roz) / (rdz + eps)
 
-    # Ручной min/max без использования встроенных функций
     tmin_x = tx1 if tx1 < tx2 else tx2
     tmax_x = tx1 if tx1 > tx2 else tx2
     tmin_y = ty1 if ty1 < ty2 else ty2
@@ -24,7 +22,6 @@ def intersect_cube(rox, roy, roz, rdx, rdy, rdz, size):
     tmin_z = tz1 if tz1 < tz2 else tz2
     tmax_z = tz1 if tz1 > tz2 else tz2
 
-    # Находим интервал пересечения
     near = tmin_x
     if tmin_y > near:
         near = tmin_y
@@ -40,11 +37,9 @@ def intersect_cube(rox, roy, roz, rdx, rdy, rdz, size):
     if near > far or far < 0:
         return -1.0, 0.0, 0.0, 0.0
 
-    # Определяем нормаль
     hx, hy, hz = rox + rdx * near, roy + rdy * near, roz + rdz * near
     nx, ny, nz = 0.0, 0.0, 0.0
 
-    # Используем простую проверку вместо abs()
     d = 1e-3
     if hx > size - d:
         nx = 1.0
@@ -66,33 +61,23 @@ def intersect_cube(rox, roy, roz, rdx, rdy, rdz, size):
 def render_frame(RES, t, cx, cy, cz, fx, fy, fz, rx, ry, rz, ux, uy, uz, x, y):
     img = np.zeros((RES, RES, 3), dtype=np.float32)
 
-    # Вращение (разные скорости для объема)
     ay, ax = t * 1.3, t * 0.8
     sy, cy_m = np.sin(ay), np.cos(ay)
     sx, cx_m = np.sin(ax), np.cos(ax)
 
-    # Три источника света (статичные, мировые)
-    # 1. Основной (ярко-белый)
     l1x, l1y, l1z = 0.5, 0.8, 0.4
-    # 2. Боковой (сероватый)
     l2x, l2y, l2z = -0.8, 0.1, 0.1
-    # 3. Контровой (слабый)
     l3x, l3y, l3z = 0.1, -0.9, -0.2
 
     for i in prange(RES):
         for j in range(RES):
-            # Вектор луча
             rdx_w = fx + x[0, j] * rx + y[i, 0] * ux
             rdy_w = fy + x[0, j] * ry + y[i, 0] * uy
             rdz_w = fz + x[0, j] * rz + y[i, 0] * uz
             rn = (rdx_w**2 + rdy_w**2 + rdz_w**2) ** 0.5
             rdx, rdy, rdz = rdx_w / rn, rdy_w / rn, rdz_w / rn
-
-            # Трансформация в локальные координаты (Y затем X)
-            # Вращаем луч
             tx, tz = rdx * cy_m + rdz * sy, -rdx * sy + rdz * cy_m
             rdx_l, rdy_l, rdz_l = tx, rdy * cx_m + tz * sx, -rdy * sx + tz * cx_m
-            # Вращаем камеру
             tcx, tcz = cx * cy_m + cz * sy, -cx * sy + cz * cy_m
             rox_l, roy_l, roz_l = tcx, cy * cx_m + tcz * sx, -cy * sx + tcz * cx_m
 
@@ -101,18 +86,14 @@ def render_frame(RES, t, cx, cy, cz, fx, fy, fz, rx, ry, rz, ux, uy, uz, x, y):
             )
 
             if dist > 0:
-                # Вращаем нормаль обратно в мир
-                nx_t, nz_t = nx, ny * sx + nz * cx_m  # Сначала X
+                nx_t, nz_t = nx, ny * sx + nz * cx_m
                 ny_t = ny * cx_m - nz * sx
 
-                nx_w = nx_t * cy_m - nz_t * sy  # Затем Y
+                nx_w = nx_t * cy_m - nz_t * sy
                 ny_w = ny_t
                 nz_w = nx_t * sy + nz_t * cy_m
 
-                # Освещение
-                bright = 0.12  # Базовая освещенность
-
-                # Добавляем свет от источников
+                bright = 0.12
                 dot1 = nx_w * l1x + ny_w * l1y + nz_w * l1z
                 if dot1 > 0:
                     bright += dot1 * 0.7
@@ -125,13 +106,11 @@ def render_frame(RES, t, cx, cy, cz, fx, fy, fz, rx, ry, rz, ux, uy, uz, x, y):
                 if dot3 > 0:
                     bright += dot3 * 0.15
 
-                # Обводка ребер (темная кайма)
                 hx, hy, hz = (
                     rox_l + rdx_l * dist,
                     roy_l + rdy_l * dist,
                     roz_l + rdz_l * dist,
                 )
-                # Берем макс. координату (абсолютную)
                 axc = hx if hx > 0 else -hx
                 ayc = hy if hy > 0 else -hy
                 azc = hz if hz > 0 else -hz
@@ -161,13 +140,11 @@ def main():
         dt = time.time() - last_t
         last_t, t = time.time(), t + dt
 
-        # Параметры камеры
         cx, cy, cz = 0.0, 0.0, 5.0
         f = np.array([0.0, 0.0, -1.0])
         r = np.array([1.0, 0.0, 0.0])
         u = np.array([0.0, 1.0, 0.0])
 
-        # Распаковываем массивы в отдельные float (для безопасности)
         frame = render_frame(
             RES,
             t,
